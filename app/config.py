7a96e77f0454c7e6ff2258a625789489146739d6
saboreje <arq.rene.salgado@gmail.com>
2024-09-05 22:00:22 -0600

    db actualizacion de URL

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:12345@172.18.112.1:5432/DBPopcornHour'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.zmiigpbhudluulpmiioe:Rs82821301+@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'b9a89ca1158c11cacb8339211eed0ba0'
     JWT_ALGORITIM = 'HS256'
