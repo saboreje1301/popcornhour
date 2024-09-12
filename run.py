e7874666668d2dd8a55d8620d0731e2a06522a3a
saboreje <arq.rene.salgado@gmail.com>
2024-09-11 22:34:16 -0600

    modificacion a entorno Heroku

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import crear_app
+import os
 
 app = crear_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Heroku asigna dinámicamente un puerto
+    app.run(host="0.0.0.0", port=port)
